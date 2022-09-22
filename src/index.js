4a1047f54c4d70fb1be656a3e3ae37803f9643b6
Alexander O'Neill <aoneill@upei.ca>
2022-09-22 17:08:20 -0300

    WIP UPdate to add basic Drupal integration.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import textOverlayPlugin from 'mirador-textoverlay/es';
 
-const config = {
-  id: 'demo',
-  windows: [{
-    imageToolsEnabled: true,
-    imageToolsOpen: true,
-    manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
-  }],
-  theme: {
-    palette: {
-      primary: {
-        main: '#1967d2',
-      },
-    },
-  },
-};
+window.Mirador = Mirador;
+window.textOverlayPlugin = textOverlayPlugin;
 
-Mirador.viewer(config, [
-  ...miradorImageToolsPlugin,
-]);
