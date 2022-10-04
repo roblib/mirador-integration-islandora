952e4ea306944e5191e45e10e416a5879439491f
Alexander O'Neill <aoneill@upei.ca>
2022-10-04 16:08:34 -0300

    Webpack compile to single chunk.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist/',
   },
+  mode: 'production',
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
+
 };
