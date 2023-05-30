63bbba0c02e22f1d3f51edc4b8486ff598fce27f
Adam Vessey <adam@discoverygarden.ca>
2023-05-30 18:44:41 -0300

    Inject plugin info by default.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import textOverlayPlugin from 'mirador-textoverlay/es';
 
-window.Mirador = Mirador;
-
 window.miradorPlugins = [
-  ...miradorImageToolsPlugin,...textOverlayPlugin,
+  ...miradorImageToolsPlugin,
+  ...textOverlayPlugin,
 ];
+
+// Monkey-patch our plugins in, by default.
+Mirador.original_viewer = Mirador.viewer;
+Mirador.islandora_viewer = function (config, pluginsOrStruct) {
+  if (typeof pluginOrStruct !== 'undefined') {
+    return Mirador.original_viewer(config, pluginsOrStruct);
+  }
+  else {
+    return Mirador.original_viewer(config, {
+      plugins: window.miradorPlugins
+    });
+  }
+};
+Mirador.viewer = Mirador.islandora_viewer;
+
+window.Mirador = Mirador;
